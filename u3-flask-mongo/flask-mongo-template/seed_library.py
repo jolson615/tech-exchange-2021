dcdfb6392b0b5b58f67ef1929f1aaebab2400e1b
dunnoconnor <moconnor8@gmail.com>
2021-11-13 20:00:15 -0500

    lesson complete

---
+++ u3-flask-mongo/flask-mongo-template/seed_library.py
@@ -0,0 +1,7 @@
+#list of books for local database
+seed_books = [
+        {"title":"The Fifth Season", "author":"N.K Jemisin", "genre":"fantasy", "publication":2015},
+        {"title":"Annihilation", "author":"Jeff VanderMeer", "genre":"science-fiction", "publication":2014},
+        {"title":"Ancillary Justics", "author":"Ann Leckie", "genre":"science-fiction", "publication":2013},
+        {"title":"Oryx and Crake", "author":"Margaret Atwood", "genre":"speculative-fiction", "publication":2003},
+]
