7b4918b4bf35639614db287c60a4447cd5afab4a
Michael Dunn-OConnor <37776449+dunnoconnor@users.noreply.github.com>
2021-11-14 20:31:46 -0500

    Update app.py

--- u3-flask-mongo/advanced-mongodb-flask/flask-mongo-template/app.py
+++ u3-flask-mongo/advanced-mongodb-flask/flask-mongo-template/app.py
@@ -14,8 +14,7 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'database'
 
 # URI of database
-app.config['MONGO_URI'] = "mongodb+srv://feedbackLoop:TestUserPassword1!@cluster0.0nsou.mongodb.net/database?retryWrites=true&w=majority"
-
+app.config['MONGO_URI'] = '<mongo-uri>'
 #Initialize PyMongo
 mongo = PyMongo(app)
 
